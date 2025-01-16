09b58f8c7a2277e7563869ff883b7147fdec94a8
Kelvin Costa <okelvincosta@gmail.com>
2025-01-16 18:24:08 -0300

    💥 fix: Img-side-card

--- src/js/components/img-side-card.js
+++ src/js/components/img-side-card.js
@@ -1,25 +1,25 @@
 export default {
-    name: "ImgSideCard",
-    props:{
-      src: String,
-      alt: String,
-      card_class: String,
-    },
-    template: //html
-     `
-    <div class="img-side-card">
-      <img
-        class="img-rounded img--purple-shadow"
-        :src="src"
-        :alt="alt"
-        loading="lazy"
-      />
-  
-      <div  :class="[card_class]" class="card card--purple-shadow" >
-        <div class="card-content">
-          <slot></slot>
-        </div>
+  name: "ImgSideCard",
+  props:{
+    src: String,
+    alt: String,
+    card_class: String,
+  },
+  template: //html
+   `
+  <div class="img-side-card">
+    <img
+      class="img-rounded img--purple-shadow"
+      :src="src"
+      :alt="alt"
+      loading="lazy"
+    />
+
+    <div  :class="[card_class]" class="card card--purple-shadow mt-24-mobile" >
+      <div class="card-content">
+        <slot></slot>
       </div>
     </div>
-    `,
-  };
+  </div>
+  `,
+};
