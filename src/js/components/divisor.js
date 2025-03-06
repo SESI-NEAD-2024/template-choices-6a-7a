e5d1daa3075ef4a407208719d600d8cc0bf9ee5e
Kelvin Costa <okelvincosta@gmail.com>
2025-03-06 10:29:25 -0300

    ✨ feat: instructions divisor componente

--- src/js/components/divisor.js
+++ src/js/components/divisor.js
@@ -1,8 +1,13 @@
 export default {
     name: "Divisor",
+    /**
+     * Componente Divisor que aceita classes modificadoras:
+     * - divisor--mbot: adiciona margem inferior
+     * - divisor--mtop: adiciona margem superior
+     */
     props:{
       src: String,
-      d_class: String,
+      d_class: String, // d_class: "divisor--mbot" ou "divisor--mtop"
     },
     template: //html
      `
